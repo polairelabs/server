6850534445886a05871fa0b49f1a0b12c1cf1892
Abdulrahim Mansour <amansour@coveo.com>
2021-11-20 17:47:56 -0500

    added new endpoint to fetch data from text index, invalidate hyphen as delimiter for each keyword in query

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # server
+
+# connecting to mongodb via shell
+
+- mac: install mongosh on homebrew
+- run command: mongosh "<ATLAS_URI>"
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5001
 
 const mongo = require('./db/conn');
+const orginBaseUrl = 'http://localhost:3000';
 
 mongo.connectToServer(()=>{
     console.log("Successfully connected to MongoDB.");
@@ -10,13 +11,46 @@ mongo.connectToServer(()=>{
 
 app.get('/shoes/random', async (req, res) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3004');
+    res.setHeader('Access-Control-Allow-Origin', orginBaseUrl);
 
     const database = mongo.getDb();
 
     if (database) { 
         database.collection("nikeshoes")
         .aggregate([{ $sample: { size: req.query.count? parseInt(req.query.count) : 10 } }])
+        .toArray(function (err, result) {
+            if (err) {
+                res.status(400).send("Error fetching shoes!");
+            } else {
+                res.json(result);
+            }
+        });
+    }
+})
+
+app.get('/shoes/search', async (req, res) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', orginBaseUrl);
+
+    const database = mongo.getDb();
+
+    if (database) { 
+        var query = req.query.query? req.query.query : "";
+
+        if (query) {
+            query = query.replace(/\s\s+/g, ' ');
+            var terms = query.split(' ');
+            terms.forEach((term, i, arr) => {
+                if (term.includes('-')) {
+                    arr[i] = term.split('-')[0];
+                }
+            })
+            query = terms.join(' ');
+        }
+
+        database.collection("nikeshoes")
+        .find({$text: {$search: query}})
+        .limit(5)
         .toArray(function (err, result) {
             if (err) {
                 res.status(400).send("Error fetching shoes!");
